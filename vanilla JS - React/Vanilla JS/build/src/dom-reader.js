078af87e0c7da34013d4e0d669eb5e8fc6f03f59
Virendra patel <patelvirendra62@gmail.com>
2022-11-26 23:51:16 +0530

    branch cleanup

--- vanilla JS - React/Vanilla JS/build/src/dom-reader.js
+++
@@ -1,3 +0,0 @@
-export const form = document.querySelector('form');
-export const tableBody = document.querySelector('#contact-list table tbody');
-//# sourceMappingURL=dom-reader.js.map
--- vanilla JS - React/Vanilla JS/build/src/utils/localStorage.d.ts
+++
@@ -1,3 +0,0 @@
-import { Contact } from '../models/Contact.js';
-export declare const saveContacts: (contacts?: Contact[]) => void;
-export declare const getContacts: () => Contact[];
