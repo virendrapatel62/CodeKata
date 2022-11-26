d6379f90b8d94b44f614a1ec865e63c6486a5d10
Virendra patel <patelvirendra62@gmail.com>
2022-11-26 21:56:06 +0530

    left only project

--- youtube-live-with-anuj/18-nov/json.js
+++
@@ -1,8 +0,0 @@
-const person = {
-  name: "virendra",
-  age: 24,
-  friendName: "Anuj",
-};
-const personToJson = JSON.stringify(person);
-console.log(personToJson);
-console.log(JSON.parse(personToJson));
