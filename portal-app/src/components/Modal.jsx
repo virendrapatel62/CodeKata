655e091cf6aeb1c758dab5d941789cd30a6d4172
Virendra patel <patelvirendra62@gmail.com>
2022-12-17 14:19:52 +0530

    react poratl use case

--- portal-app/src/components/Modal.jsx
+++ portal-app/src/components/Modal.jsx
@@ -1,41 +1,49 @@
 import React, { Fragment } from "react";
+import { createPortal } from "react-dom";
 import Modal from "react-bootstrap/Modal";
+import { useEffect } from "react";
 
 function MyAppModal({ show, onHide, children, title }) {
+  useEffect(() => {
+    document.getElementById("root").setAttribute("aria-hidden", show);
+  }, [show]);
   return show ? (
-    <div
-      style={{
-        width: "100vw",
-        minHeight: "100vh",
-        top: 0,
-        right: 0,
-        left: 0,
-        zIndex: 100,
-        position: "absolute",
-        backgroundColor: "white",
-      }}
-    >
-      <div className="container mt-4">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "end",
-            fontSize: 40,
-          }}
-        >
-          <span
-            onClick={onHide}
+    createPortal(
+      <div
+        style={{
+          width: "100vw",
+          minHeight: "100vh",
+          top: 0,
+          right: 0,
+          left: 0,
+          zIndex: 100,
+          position: "absolute",
+          backgroundColor: "white",
+        }}
+      >
+        <div className="container mt-4">
+          <div
             style={{
-              cursor: "pointer",
+              display: "flex",
+              justifyContent: "end",
+              fontSize: 40,
             }}
           >
-            ✖️
-          </span>
+            <span
+              onClick={onHide}
+              style={{
+                cursor: "pointer",
+              }}
+            >
+              ✖️
+            </span>
+          </div>
+          <h2>{title}</h2>
+          <div>{children}</div>
         </div>
-        <h2>{title}</h2>
-        <div>{children}</div>
-      </div>
-    </div>
+      </div>,
+      document.querySelector("#modal-root")
+    )
   ) : (
     <Fragment />
   );
